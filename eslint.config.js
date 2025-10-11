d6529e3f271addfc9fd66d826b9bbca333dcb43d
Joseph Haase <joseph.haase.dev@gmail.com>
2025-10-11 19:58:00 -0300

    chore: Adiciona nova fonte e ajusta os breakpoints de responsividade

--- eslint.config.js
+++ eslint.config.js
@@ -32,6 +32,7 @@ export default tseslint.config([
 
       'react-hooks/rules-of-hooks': 'off',
       'react-hooks/exhaustive-deps': 'off',
+      'react-refresh/only-export-components': 'warn',
     },
   },
   {
