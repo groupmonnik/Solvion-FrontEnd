752b3f6f63b079a311a5b8153e7bd8abdb871ab2
Joseph Haase <joseph.haase.dev@gmail.com>
2025-10-23 14:52:16 -0300

    refactor(sign_in): Ajusta as mensagens de feedback do formulário

--- src/pages/public/SignIn/schemas.ts
+++ src/pages/public/SignIn/schemas.ts
@@ -5,24 +5,24 @@ export const formSchema = z.object({
   email: z
     .string()
     .min(1, {
-      message: 'Campo obrigatório.',
+      message: 'Campo obrigatório',
     })
     .max(120, {
-      message: 'O campo deve ter no máximo 120 caracteres.',
+      message: 'O campo deve ter no máximo 120 caracteres',
     })
     .regex(sysConstants.regex.VALID_EMAIL_REGEX, {
-      message: 'E-mail inválido.',
+      message: 'E-mail inválido',
     }),
   password: z
     .string()
     .min(1, {
-      message: 'Campo obrigatório.',
+      message: 'Campo obrigatório',
     })
     .max(500, {
-      message: 'O campo deve ter no máximo 500 caracteres.',
+      message: 'O campo deve ter no máximo 500 caracteres',
     })
     .regex(sysConstants.regex.PASSWORD_STRONG_REGEX, {
       message:
-        'Senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&).',
+        'Senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&)',
     }),
 });
